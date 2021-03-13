0139f9d6b92605a4017495fcab5d2d2fa50b4780
Ana-Costa12 <claudiasantborges@gmail.com>
2021-03-12 21:58:46 -0300

    Cebola Tradutor

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aula_de_PC1_FalaCebola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cebolatradutor;
+
+            Console.Write("Escreva algo para o CebolaTradutor:\n ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            cebolatradutor = Console.ReadLine();
+            Console.ResetColor();
+            Console.Clear();
+
+            Console.WriteLine("Tecle enter para ver sua tradução. ");
+            Console.ReadLine();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"{cebolatradutor.Replace("r","l").Replace("R","L")}");
+            Console.ResetColor();
+        }
+    }
+}
